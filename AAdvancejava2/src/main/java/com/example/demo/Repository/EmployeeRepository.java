99d934fc77bab911bdfa82778b050ec935b3da9a
vedan masane <vedantmasane10@gmail.com>
2025-07-24 15:37:21 +0530

    Removed Spring Boot project: AAdvancejava2

--- AAdvancejava2/src/main/java/com/example/demo/Repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entity.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
-}
